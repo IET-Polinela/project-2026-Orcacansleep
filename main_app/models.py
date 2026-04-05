7ef8aa48ebbfd45a833afa2173386c94cb6d0a4a
Orcacansleep <nazheefzihni07@gmail.com>
2026-04-05 15:03:21 +0700

    LabSession3:Integrasi PostgreSQL dan CRUD Report

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Report(models.Model):
+    title = models.CharField(max_length=200)  # Judul laporan
+    category = models.CharField(max_length=100)  # Kategori laporan
+    description = models.TextField()  # Deskripsi laporan
+    location = models.CharField(max_length=200)  # Lokasi laporan
+    status = models.CharField(
+        max_length=20,
+        default='REPORTED',  # Status default
+    )
+    created_at = models.DateTimeField(auto_now_add=True)  # Tanggal pembuatan laporan
+
+    def __str__(self):
+        return self.title
